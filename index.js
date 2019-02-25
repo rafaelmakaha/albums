a0e37d25dc28b8e20c24cf3b9d772a713fa734de
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2019-02-25 18:51:15 -0300

    Deletes Apps file and use them inside Index.js.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => {
+    return(
+        <Text>Some text</Text>
+    )
+}
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('albums', () => App);
