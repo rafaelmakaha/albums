a17cd57ac395b08813cbecd87460e374aca0ef4e
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2019-02-25 18:51:57 -0300

    Creates Header component.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+const Header = () => {
+    return (
+        <Text>Albums</Text>
+    )
+};
+
+export default Header;
