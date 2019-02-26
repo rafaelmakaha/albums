e86c86870f4a8cee7287b27b0a337fa2765daa69
Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>
2019-02-26 14:53:00 -0300

    Adds styles to header component.
    Signed-off-by: Rafael Makaha Gomes Ferreira <rafael.makaha@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native'
+import React from 'react'
+import { Text, View } from 'react-native'
 
 const Header = () => {
+    const { textStyle, viewStyle } = styles;
+
     return (
-        <Text>Albums</Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
     )
 };
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#4286F4',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative',
+    },
+    textStyle: {
+        fontSize: 20,
+    },
+}
 
 export default Header;
